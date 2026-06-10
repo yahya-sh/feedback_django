3e4982e809c9773cab8dca0dbf3320b828847dbb
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-10 15:03:04 +0300

    added the Review model and save the submitted review form into db

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core import validators
+
 
 # Create your models here.
+class Review(models.Model):
+    username = models.CharField(
+        max_length=255,
+        validators=[
+            validators.MinLengthValidator(3),
+        ],
+    )
+    feedback = models.TextField()
+    rating = models.IntegerField(
+        validators=[
+            validators.MinValueValidator(1),
+            validators.MaxValueValidator(5),
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.username} (Rate {self.rating} of 5)"
--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from . import forms
-
+from . import models
 
 # Create your views here.
 def review(request):
     form = forms.ReviewForm(request.POST if request.method == "POST" else None)
     if request.method == "POST":
         if form.is_valid():
-            print(form.cleaned_data)
+            review = models.Review(**form.cleaned_data)
+            review.save()
             return redirect("thank-you")
     return render(
         request,
