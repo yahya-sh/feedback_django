ac17ee3d79a9284495599366de83dbc39289c0a6
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-10 11:25:04 +0300

    added feedback and rating for review form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+
 class ReviewForm(forms.Form):
     username = forms.CharField(min_length=3)
-
+    feedback = forms.CharField(max_length=150, widget=forms.Textarea)
+    rating = forms.IntegerField(max_value=5, min_value=1)
