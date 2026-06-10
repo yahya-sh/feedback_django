33cfdfa02c26387104771f856b1a876a08d8ba1b
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-10 15:28:21 +0300

    replace Review Form with ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from . import models
+
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(min_length=3)
+#     feedback = forms.CharField(max_length=150, widget=forms.Textarea)
+#     rating = forms.IntegerField(max_value=5, min_value=1)
 
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(min_length=3)
-    feedback = forms.CharField(max_length=150, widget=forms.Textarea)
-    rating = forms.IntegerField(max_value=5, min_value=1)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = "__all__"
+        
