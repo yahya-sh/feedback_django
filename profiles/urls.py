091349774ca5281d4c4b9d477fd798ce46cb2639
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-11 14:47:16 +0300

    implement uploading image using manual python way

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "profiles"
+
 urlpatterns = [
-    path("", views.CreateProfileView.as_view())
+    path("", views.CreateProfileView.as_view(), name="upload"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views import View
 
 # Create your views here.
+
+
+def save_jpg_image(image):
+    try:
+        with open(f"temp/{image.name}", "wb") as dest:
+            for chunk in image.chunks():
+                dest.write(chunk)
+        return True
+    except Exception:
+        return False
 
 
 class CreateProfileView(View):
@@ -9,4 +20,8 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        image = request.FILES["image"]
+        print(image)
+        print(type(image))
+        save_jpg_image(image)
+        return redirect("profiles:upload")
