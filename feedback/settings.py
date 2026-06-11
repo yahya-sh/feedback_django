5b9f5484d69c933d8602b06cc424d96581a391bb
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-11 14:15:54 +0300

    added profiles app

--- feedback/settings.py
+++ feedback/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "reviews",
+    "profiles",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = "feedback.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "feedback" / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
